ad9a03b1f45b9e80241b0973666da1e705c7c48a
malewis5 <matthew.a.lewis7@gmail.com>
2024-05-20 09:18:45 -0400

    fix: add comments

--- sanity/schemas/objects/custom-components.ts
+++ sanity/schemas/objects/custom-components.ts
@@ -1,5 +1,11 @@
 import { defineType } from 'sanity'
 
+/** 
+This type is referenced in the page schema. The array is used to define which components 
+can be added to the body of the page. Using this custom type allows us to add our custom components
+to one schema and have them be referenced by multiple schemas.
+*/
+
 export default defineType({
   name: 'customComponents',
   title: 'Custom Components',
