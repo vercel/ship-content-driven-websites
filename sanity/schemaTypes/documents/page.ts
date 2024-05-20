a8fff1e7ff5b4dbe7fb574564dd6e54d6fb66973
Knut Melvær <knut@sanity.io>
2024-05-20 14:14:34 -0700

    improve ex

--- sanity/schemaTypes/documents/page.ts
+++ sanity/schemaTypes/documents/page.ts
@@ -1,6 +1,5 @@
 import { DocumentIcon, ImageIcon } from '@sanity/icons'
 import { defineArrayMember, defineField, defineType } from 'sanity'
-
 export const page = defineType({
   type: 'document',
   name: 'page',
@@ -10,39 +9,43 @@ export const page = defineType({
     defineField({
       type: 'string',
       name: 'title',
-      title: 'Title',
+      title: 'Page title',
       validation: (rule) => rule.required(),
     }),
     defineField({
       type: 'slug',
       name: 'slug',
-      title: 'Slug',
+      title: 'Page slug',
       options: {
         source: 'title',
       },
-      validation: (rule) => rule.required(),
+      validation: (rule) => rule.required().error("You need to set a slug to publish/preview the page"),
     }),
     defineField({
       name: 'description',
       description: 'Used for the <meta> description tag for SEO.',
-      title: 'Description',
+      title: 'Page description',
       type: 'text',
-      validation: (rule) => rule.max(155).required(),
+      validation: (rule) => [
+        rule.required().error('Please provide a description for the page. '),
+        rule.max(155).warning('Meta descriptions should be between 50 and 155 characters or else they can get truncated in search results.'),
+      ],
     }),
     defineField({
       type: 'customComponents',
       name: 'body',
-      title: 'Body',
+      title: 'Page body',
       description: "This is where you can create the page's content.",
     }),
   ],
   preview: {
     select: {
       title: 'title',
+      slug: 'slug.current'
     },
-    prepare({ title }) {
+    prepare({ title, slug }) {
       return {
-        subtitle: 'Page',
+        subtitle: slug ? `/${slug}` : 'No slug set',
         title,
       }
     },
