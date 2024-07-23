9afbbc78f85a45c4c6d3e5e9f1ac81f315f5be8d
malewis5 <matthew.a.lewis7@gmail.com>
2024-07-23 15:33:13 -0400

    fix light mode verce icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -10,7 +10,7 @@ export const Icons = {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#ffffff" />
+      <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="currentColor" />
     </svg>
   ),
   twitter: (props: IconProps) => (
